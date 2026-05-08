a7a8c7779f5ee9d46dc71bd7d02281e9b16e4b19
Sethin Ginige <sething.ginige@gmail.com>
2026-05-08 13:18:15 +1000

    API Change

--- src/app/api/hello/route.jsx
+++ src/app/api/hello/route.jsx
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
+import ApiProxy from "../proxy";
 import { DJANGO_API_ENDPOINT } from "@/config/defaults";
 
-const DJANG_API_QUESTIONS_URL = `${DJANGO_API_ENDPOINT}/questions/`
 
-export async function GET(request) {
+export async function GET(request){
     const data = {apiEndpoint: DJANGO_API_ENDPOINT}
     return NextResponse.json(data, {status: 200})
 }
--- src/app/page.js
+++ src/app/page.js
@@ -11,12 +11,11 @@ const fetcher = (...args) => fetch(...args).then(res => res.json())
 export default function Home() {
   const auth = useAuth()
   const {data, error, isLoading} = useSWR("/api/hello", fetcher)
-  if (error) return <div>failed to load</div>
-  if (isLoading) return <div>loading...</div>
 
 
   return (
     <main className="min-h-screen bg-muted p-6 md:p-10">
+      <div>{data && data.apiEndpoint}</div>
          {<Questions/>}
      </main>
   );
