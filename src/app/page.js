c1ea5faeb26dceb9619aa41fa07d48e2a95a4753
Sethin Ginige <sething.ginige@gmail.com>
2026-05-08 13:46:20 +1000

    Fixed Routing

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,7 @@
 "use client"
-import { useAuth } from "@/components/authProvider";
-import { ThemeToggleButton } from "@/components/ui/themeToggleButton";
-import Image from "next/image";
-import {useState} from 'react';
-import useSWR from 'swr';
 import Questions from "./questions/forms";
 
-const fetcher = (...args) => fetch(...args).then(res => res.json())
-
 export default function Home() {
-  const auth = useAuth()
-  const {data, error, isLoading} = useSWR("/api/hello", fetcher)
-
 
   return (
     <main className="min-h-screen bg-muted p-6 md:p-10">
